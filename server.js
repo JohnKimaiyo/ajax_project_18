6d673a4cdf4e5db95df595ad4532983153d52b84
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-09 10:46:42 +0300

    seting up the server

--- server.js
+++ server.js
@@ -1,87 +1,28 @@
-const { Router } = require('express');
 const express = require('express');
-const router = express.Router();
-// employeee data
-let employees = [
-    {
-        id: '_a',
-        first_name: 'john',
-        last_name: 'wilson',
-        email: 'john@gmail.com',
-        gender: 'male',
-        ip_address: '127.0.0.1'
-    },
-    {
-        id: '_b',
-        first_name: 'janet',
-        last_name: 'wilson',
-        email: 'janet@gmail.com',
-        gender: 'female',
-        ip_address: '127.0.0.1'
-    },
-]
-// get id
-let getID = () => {
-    return '_' + Math.random().toString(36).substr(2, 9);
-}
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./router');
 
-// get employee
-router.get('/employee', (request, response) => {
-    response.json(employee);
+const hostname = '127.0.0.1';
+
+// configure bodyparser
+const jsonParser = bodyParser.json();
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+app.use(jsonParser);
+app.use(urlEncodedParser);
+
+// configure cors
+app.use(cors());
+
+// configure the router
+app.use('/api', router)
+
+// get
+app.get('/', (request, response) => {
+    response.send(`<h2>welcome to express of employee portals </h2>`);
 });
 
-// post request
-router.post('/employee', (request, response) => {
-    let employee = {
-        id: getID(),
-        first_name: request.body.first_name,
-        last_name: request.body.last_name,
-        email: request.body.email,
-        gender: request.body.gender,
-        ip_address: request.body.ip_address
-    }
-    employee.push(employee);
-    console.log(`POST request received at server ..${new Date().toLocaleTimeString()}`);
-    response.jsonp(employee);
+app.listen(port, hostname, () => {
+    console.log(`express server is live at http://${hostname} :${port}`);
 });
-
-// put requets
-router.put('/employees/:id', (request, response) => {
-    let empID = request.params.id;
-    let updateEmployee = {
-        id: empID,
-        first_name: request.body.first_name,
-        last_name: request.body.last_name,
-        email: request.body.gender,
-        ip_address: request.body.ip_address
-    }
-    let existingEmployee = employee.find((employee) => {
-        return employee.id === empID;
-    });
-    employees.splice(employee.indexOf(existingEmployee), 1, updateEmployee);
-    console.log(`PUT request received at server ..${new Date().tolacaleTimestring()}`);
-    response.json({ msg: 'PUT request is a success' });
-});
-
-// delete request
-router.delete('/employee/:id', (request, response) => {
-    let empID = request.params.id;
-    employees = employees.filter((employee) => {
-        return employee.id !== empID;
-    });
-    console.log(`delete requeste received at server ..${new Date().toLocaleTimeString()}`);
-    response.json({ msg: 'delete request is a success' });
-});
-module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-

574fbfa000300f55cb26d56836553a96329def82
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-18 18:29:57 +0300

    setting up put and delete request

--- app.js
+++ app.js
@@ -45,3 +45,36 @@ postButton.addEventListener('click', function () {
         fetchEmmployees();
     })
 })
+
+// put request
+let putButton = document.querySelector('#put-btn');
+putButton.addEventListener('click', function () {
+    let empID = '_abcde';
+    let employee =
+    {
+        id: empID,
+        first_name: 'john',
+        last_name: 'wislon',
+        email: 'johnwilson@gmail',
+        gender: 'male',
+        ip_address: 255
+    }
+    let url = `${serverURL}/employee/${empID}`;
+    let http = new brainhttp();
+    http.put(url, employee, (data) => {
+        alert(JSON.stringify(data));
+        fetchEmployees
+    })
+})
+
+// delete button
+let deleteButton = document.querySelector('#delete-btn');
+deleteButton.addEventLsitener('click', function () {
+    let employeeID = `_vwxyz`
+    let url = `${serverURL}/employees`;
+    let http = new brainhttp();
+    http.delete(url, (data) => {
+        alert(JSON.stringify(data));
+        fetchEmployees();
+    })
+})
--- brainhttp.js
+++ brainhttp.js
@@ -32,3 +32,28 @@ post = (url, employees, callback) => {
         callback(employees);
     }
 }
+
+// put request
+put = (url, employees, callback) => {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('content-Type', 'application/json');
+    this.http.send(JSON.stringify(employees));
+    this.http.onload = () => {
+        let data = this.http.responseText;
+        let employees = JSON.parse(data)
+        callback(employees);
+    }
+}
+
+// delete request
+delet = (url, callback) => {
+    this.http.open('DELETE', url, true);
+    this.http.setRequestHeader('content-Type', 'application/json');
+    this.http.send();
+    this.http.onload = () => {
+        let data = this.http.responseText;
+        let employee = JSON.parse(data)
+        callback(employees);
+    }
+
+}
